f807d253a0099029f8ece15fa70caace93e91b98
Tigran Farmanyan <tigran.farmanyan@gmail.com>
2024-03-28 22:08:02 -0500

    Sidebar component style and add navigation links

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,16 +1,66 @@
-import Image from 'next/image'
-import Link from 'next/link'
+"use client";
+
+import { navLinks } from "@/constants";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
-    <aside className='sidebar'>
-        <div className="flex size-full flex-col gap-4">
-            <Link href="/" className='sidebar-logo'>
-                <Image src="/assets/images/logo-text.webp" alt="PixelAI Studio" width={180} height={28} />
-            </Link>
-        </div>
-    </aside>
-  )
-}
+    <aside className="sidebar">
+      <div className="flex size-full flex-col gap-4">
+        <Link href="/" className="sidebar-logo">
+          <Image
+            src="/assets/images/logo-text.webp"
+            alt="PixelAI Studio"
+            width={180}
+            height={28}
+          />
+        </Link>
 
-export default Sidebar
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.map((link) => {
+                const isActive = pathname === link.route;
+
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-gradient-to-br from-dark-400 to-dark-600 text-slate-50" 
+                        : "text-slate-700"
+                    }`}
+                  >
+                    <Link
+                      href={link.route}
+                      className="sidebar-link"
+                    >
+                      <Image
+                        src={link.icon}
+                        alt={link.label}
+                        width={24}
+                        height={24}
+                        className={`${isActive && 'brightness-200'}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </SignedIn>
+
+          <SignedOut>
+            
+          </SignedOut>
+        </nav>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
